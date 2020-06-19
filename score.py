8e7d1b0f875e073e59cd90206cf8a3aa55236fbb
LawJarp <prajwalanagani@gmail.com>
2020-06-19 17:07:37 +0530

    fixed score.py

--- score.py
+++ score.py
@@ -8,22 +8,15 @@ def norm(k):
 
 
 cap = cv2.VideoCapture('vid.avi')
-# Find OpenCV version
-
-(major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
-if int(major_ver)  < 3 :
-    fps =cap.get(cv2.cv.CV_CAP_PROP_FPS)
-
-else :
-    fps = cap.get(cv2.CAP_PROP_FPS)
+fps = math.ceil(cap.get(cv2.CAP_PROP_FPS))
 
 i = 0
 kb = None
-temp = []
 flag = 0
 c = 0
 score = []
-while(cap.isOpened() and (flag!=2)):
+
+while(cap.isOpened() and flag!=2):
     ret, frame = cap.read()
     if(i == fps):
         try:
@@ -32,9 +25,8 @@ while(cap.isOpened() and (flag!=2)):
             flag+=1
             continue
         if(kb and k):
-            #Code goes here
-            try:
 
+            try:
                 lw = k[5]
                 rw = k[2]
                 Lw = kb[5]
@@ -44,14 +36,19 @@ while(cap.isOpened() and (flag!=2)):
                     distL = math.sqrt(((lw[0]-Lw[0])**2)+((lw[1]-Lw[1])**2))
                     distR = norm(distR)
                     distL = norm(distL)
-                    if(distR<0.2 or distL<0.2):
+                    print(distR,distL,"***")
+                    # if(c>fps):
+                    #     score.append(0.9)
+                    #     c = 0
+                    if(distR<0.1 or distL<0.1):
                         c+=1
+
                     else:
-                        if(c>100):
+                        if(c>fps):
                             score.append(0.9)
-                        elif(c in range(80,100)):
+                        elif(c in range(fps-10,fps)):
                             score.append(0.8)
-                        elif(c in range(40,80)):
+                        elif(c in range(fps-20,fps-10)):
                             score.append(0.5)
                         else:
                             score.append(0.2)
@@ -67,6 +64,7 @@ while(cap.isOpened() and (flag!=2)):
     i+=1
     if  0xFF == ord('q'):
         break
+
 # print(temp)
 # print(c)
 # print(fps)
